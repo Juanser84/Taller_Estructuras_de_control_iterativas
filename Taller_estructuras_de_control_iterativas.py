5af176f45baa66ae255bc4a21e6291e0d92fc0e8
juanser84 <juanser84@gmail.com>
2026-05-18 14:36:51 -0500

    Lectura del primer valor para evaluar la condición de entrada

--- Taller_estructuras_de_control_iterativas.py
+++ Taller_estructuras_de_control_iterativas.py
@@ -8,4 +8,16 @@ mejor_rendimiento = 0.0 # Almacena el mejor rendimiento encontrado
 tanqueos_validos = 0 # Contador de tanqueos con octanaje válido
 tanqueos_extra = 0 # Contador de tanqueos con octanaje alto
 
+# Lectura del primer valor para evaluar la condición de entrada
+
+galones = float(input()) # Lectura del primer valor de galones
+
+if galones == 0.0:
+    print("No data provided.") # Caso especial: Si el primer valor es 0, no hay datos
+else:
+    while galones != 0.0: # Ciclo principal que se ejecuta mientras no se ingrese 0 galones
+        km = float(input()) # Lectura de kilómetros recorridos
+        octanaje = int(input()) # Lectura del octanaje
+        
+        
         
