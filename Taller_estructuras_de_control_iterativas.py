ec61408a43c2ef682130a0dd5d5f941c4f8e14f8
juanser84 <juanser84@gmail.com>
2026-05-18 14:35:57 -0500

    Inicialización_de_variables_acumuladoras

---
+++ Taller_estructuras_de_control_iterativas.py
@@ -0,0 +1,11 @@
+# Taller de estructuras de control iterativas
+
+# Inicialización de variables acumuladoras y de control
+
+total_galones = 0.0 # Acumulador para la cantidad total de galones consumidos
+total_km = 0.0 # Acumulador para la cantidad total de kilómetros recorridos
+mejor_rendimiento = 0.0 # Almacena el mejor rendimiento encontrado
+tanqueos_validos = 0 # Contador de tanqueos con octanaje válido
+tanqueos_extra = 0 # Contador de tanqueos con octanaje alto
+
+        
